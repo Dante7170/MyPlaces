416acde3cd964b0ee32718a1226f450fa062264e
Komilov Jasur <komilovjasur@yandex.ru>
2020-06-16 01:56:55 +0300

    CustomTabelViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Jasurbek Komilov on 16.06.2020.
+//  Copyright © 2020 Jasurbek Komilov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
